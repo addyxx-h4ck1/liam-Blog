16337630fcb0f321937a6de54f6431dd35f4665e
addyxx-h4ck1 <briannjosh23@gmail.com>
2024-01-16 19:11:46 +0300

    First Commit

---
+++ js/main.js
@@ -0,0 +1,12 @@
+//////sidebar//////////
+function showSidebar() {
+     const sidebar =  document.querySelector('.sidebar')
+     sidebar.style.display = 'flex'
+}
+function hideSidebar() {
+     const sidebar =  document.querySelector('.sidebar')
+     sidebar.style.display = 'none'
+}
+/////////AOS////////////
+
+/////////AOS////////////
